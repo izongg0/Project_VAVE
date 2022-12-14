8654f04607cd71ec796eb5e9dfdd89a841514b1b
Chaewon <cw001121@naver.com>
2022-12-14 20:43:59 +0900

    fis: modelback main.py

--- vavefront/src/router/index.js
+++ vavefront/src/router/index.js
@@ -1,29 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
+    name: 'login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+      import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
   },
   {
-    path: '/frame',
-    name: 'frame',
+    path: '/main',
+    name: 'main',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/Frame.vue')
+      import(/* webpackChunkName: "about" */ '../views/HomeView.vue')
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/SignupView.vue')
+  },
+  {
+    path: '/mypage',
+    name: 'mypage',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/Mypage.vue')
   }
 ]
 
